ea4dfe44b54b0bb402cb95d93fe63078744f2b7c
bhristov <magnetgroup@live.com>
2020-02-21 17:35:40 +0200

    exercise lambda improved find only string with first letter upper

--- lambda_and_build_in_functions_exercise/unique_names_7.py
+++ lambda_and_build_in_functions_exercise/unique_names_7.py
@@ -1,5 +1,5 @@
 names_input = input().split()
 
-filter_names = filter(lambda x: x[0].isupper() and x[1:].islower(), names_input)
+filter_names = filter(lambda x: x.istitle(), names_input)
 
 print(sum(map(lambda x: len(x), list(filter_names))))
