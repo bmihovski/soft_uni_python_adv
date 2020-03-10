674fd4f93a06b007e510d82b5f255c3572eebdbd
bhristov <magnetgroup@live.com>
2020-03-10 22:36:53 +0200

    lab iterators and generators first and second completed

--- iterators_and_generators_lab/reverse_iter_2.py
+++ iterators_and_generators_lab/reverse_iter_2.py
@@ -22,3 +22,7 @@ class TenToOneTests(TestCase):
     def test_zero(self):
         ten_to_one = reverse_iter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         self.assertEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], [num for num in ten_to_one])
+
+    def test_one(self):
+        empty_list = reverse_iter([])
+        self.assertEqual([], [num for num in empty_list])
