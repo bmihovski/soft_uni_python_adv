baae225c9eeb4b065f05512053ffc688059ab10c
bhristov <magnetgroup@live.com>
2020-03-10 21:43:47 +0200

    lab encapsulation completed

--- encapsulation_lab/account_4.py
+++ encapsulation_lab/account_4.py
@@ -1,11 +1,11 @@
 class Account:
     def __init__(self, id: int, balance: int, pin: int):
-        self.__id = id
+        self._id = id
         self.balance = balance
         self.__pin = pin
 
     def get_id(self, pin: int):
-        return self.__id if pin == self.__pin else "Wrong pin"
+        return self._id if pin == self.__pin else "Wrong pin"
 
     def get_balance(self):
         return self.balance
