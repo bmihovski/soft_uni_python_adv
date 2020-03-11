bb559ebf81092e2a373c100ac93a1217f9b290ab
bhristov <magnetgroup@live.com>
2020-03-11 16:34:10 +0200

    exercise encapsulation working with not refactored status

--- encapsulation_exercise/wild_cat_zoo_1.py
+++ encapsulation_exercise/wild_cat_zoo_1.py
@@ -9,7 +9,7 @@ class Lion:
         self.gender = gender
         self.age = age
 
-    def get_need(self):
+    def get_needs(self):
         return self.__lions_need
 
     def __repr__(self):
@@ -24,7 +24,7 @@ class Tiger:
         self.gender = gender
         self.age = age
 
-    def get_need(self):
+    def get_needs(self):
         return self.__tigers_need
 
     def __repr__(self):
@@ -39,7 +39,7 @@ class Cheetah:
         self.gender = gender
         self.age = age
 
-    def get_need(self):
+    def get_needs(self):
         return self.__cheetahs_need
 
     def __repr__(self):
@@ -88,18 +88,18 @@ class Zoo:
     def add_animal(self, animal: object, price: int):
         if self.__animal_capacity > 0 and self.__budget - price <= 0:
             return "Not enough budget"
-        if self.__animal_capacity <= 0:
+        if len(self.animals) == self.__animal_capacity:
             return "Not enough space for animal"
         self.animals.append(animal)
         self.__budget -= price
-        self.__animal_capacity -= 1
+        #self.__animal_capacity -= 1
         return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
 
     def hire_worker(self, worker: object):
-        if self.__workers_capacity <= 0:
+        if len(self.workers) == self.__workers_capacity:
             return "Not enough space for worker"
         self.workers.append(worker)
-        self.__workers_capacity -= 1
+        #self.__workers_capacity -= 1
         return f"{worker.name} the {worker.__class__.__name__} hired successfully"
 
     def fire_worker(self, worker_name: str):
@@ -107,7 +107,7 @@ class Zoo:
         if len(__workers_to_stay) == len(self.workers):
             return f"There is no {worker_name} in the zoo"
         self.workers = __workers_to_stay
-        self.__workers_capacity += 1
+        #self.__workers_capacity += 1
         return f"{worker_name} fired successfully"
 
     def pay_workers(self):
@@ -118,7 +118,7 @@ class Zoo:
         return f"You payed your workers. They are happy. Budget left: {self.__budget}"
 
     def tend_animals(self):
-        __total_tend_cost = reduce(lambda acc, animal_to_tend: acc + animal_to_tend.get_need(), self.animals, 0)
+        __total_tend_cost = reduce(lambda acc, animal_to_tend: acc + animal_to_tend.get_needs(), self.animals, 0)
         if self.__budget - __total_tend_cost <= 0:
             return "You have no budget to tend the animals. They are unhappy."
         self.__budget -= __total_tend_cost
@@ -141,25 +141,43 @@ class Zoo:
             results.append(each_type)
         return results
 
+    # def animals_status(self):
+    #     __lions = list(filter(self.__is_type("Lion"), self.animals))
+    #     __tigers = list(filter(self.__is_type("Tiger"), self.animals))
+    #     __cheetahs = list(filter(self.__is_type("Cheetah"), self.animals))
+    #     __total_animals_count = f"You have {len(self.animals)} animals"
+    #     __lion_results = Zoo.__generate_status(__lions)
+    #     __tiger_results = Zoo.__generate_status(__tigers)
+    #     __cheetah_results = Zoo.__generate_status(__cheetahs)
+    #     return "\n".join([__total_animals_count, *__lion_results, *__tiger_results, *__cheetah_results])
+
+    # def workers_status(self):
+    #     __keepers = list(filter(self.__is_type("Keeper"), self.workers))
+    #     __caretakers = list(filter(self.__is_type("Caretaker"), self.workers))
+    #     __vets = list(filter(self.__is_type("Vet"), self.workers))
+    #     __total_workers_count = f"You have {len(self.workers)} workers"
+    #     __keeper_results = Zoo.__generate_status(__keepers)
+    #     __caretaker_results = Zoo.__generate_status(__caretakers)
+    #     __vet_results = Zoo.__generate_status(__vets)
+    #     return "\n".join([__total_workers_count, *__keeper_results, *__caretaker_results, *__vet_results])
+
     def animals_status(self):
-        __lions = list(filter(self.__is_type("Lion"), self.animals))
-        __tigers = list(filter(self.__is_type("Tiger"), self.animals))
-        __cheetahs = list(filter(self.__is_type("Cheetah"), self.animals))
-        __total_animals_count = f"You have {len(self.animals)} animals "
-        __lion_results = Zoo.__generate_status(__lions)
-        __tiger_results = Zoo.__generate_status(__tigers)
-        __cheetah_results = Zoo.__generate_status(__cheetahs)
-        return "\n".join([__total_animals_count, *__lion_results, *__tiger_results, *__cheetah_results])
+        result = ""
+        result += f"You have {len(self.animals)} animals\n"
+        for animal_type in ("Lion", "Tiger", "Cheetah"):
+            result += f"----- {len([animal for animal in self.animals if animal_type == animal.__class__.__name__])} {animal_type}s:\n"
+            for single_animal in [animal for animal in self.animals if animal_type == animal.__class__.__name__]:
+                result += f"{repr(single_animal)}\n"
+        return result
 
     def workers_status(self):
-        __keepers = list(filter(self.__is_type("Keeper"), self.workers))
-        __caretakers = list(filter(self.__is_type("Caretaker"), self.workers))
-        __vets = list(filter(self.__is_type("Vet"), self.workers))
-        __total_workers_count = f"You have {len(self.workers)} workers"
-        __keeper_results = Zoo.__generate_status(__keepers)
-        __caretaker_results = Zoo.__generate_status(__caretakers)
-        __vet_results = Zoo.__generate_status(__vets)
-        return "\n".join([__total_workers_count, *__keeper_results, *__caretaker_results, *__vet_results])
+        result = ""
+        result += f"You have {len(self.workers)} workers\n"
+        for worker_type in ("Keeper", "Caretaker", "Vet"):
+            result += f"----- {len([worker for worker in self.workers if worker_type == worker.__class__.__name__])} {worker_type}s:\n"
+            for single_worker in [worker for worker in self.workers if worker_type == worker.__class__.__name__]:
+                result += f"{repr(single_worker)}\n"
+        return result
 
 
 zoo: Zoo = Zoo("Zootopia", 3000, 5, 8)
